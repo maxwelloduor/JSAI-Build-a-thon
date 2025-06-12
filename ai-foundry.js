1c8e865ae823807cd116ec16c6545a4330fe030b
maxwelloduor <maxwelloduor55@gmail.com>
2025-06-12 10:03:56 +0300

    Connect to Azure AI Foundry

---
+++ ai-foundry.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import ModelClient from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+  const client = new ModelClient(
+  process.env.AZURE_INFERENCE_SDK_ENDPOINT ?? "https://aistudioaiservices742846984659.services.ai.azure.com/models", new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY ?? "YOUR_KEY_HERE"));
+
+var messages = [
+  { role: "system", content: "You are an helpful assistant" },
+  { role: "user", content: "What are 3 things to see in Seattle?" },
+];
+
+var response = await client.path("chat/completions").post({
+  body: {
+    messages: messages,
+    max_tokens: 4096,
+      temperature: 1,
+      top_p: 1,
+      model: "gpt-4o",
+  },
+});
+
+console.log(JSON.stringify(response));
